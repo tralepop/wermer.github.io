9970fb35a8543ebbc2bccc5cc3e7ac2ca3db4ad5
tralepop <100528033+tralepop@users.noreply.github.com>
2022-05-19 22:26:42 +0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,34 @@
+$(function() {
+
+    var header = $("#header"),
+        failH = 0,
+        scrollOffset = 0;
+
+
+
+
+    /* Smooth scroll */
+    $("[data-scroll]").on("click", function(event) {
+        event.preventDefault();
+
+        var $this = $(this),
+            blockId = $this.data('scroll'),
+            blockOffset = $(blockId).offset().top;
+
+        $("#nav a").removeClass("active");
+        $this.addClass("active");
+
+        $("html, body").animate({
+            scrollTop:  blockOffset
+        }, 900);
+    });
+
+    /* burger menu */
+
+
+
+
+
+
+
+});
